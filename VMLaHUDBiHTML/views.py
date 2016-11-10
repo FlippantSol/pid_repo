7a9f4b896b773c97307651db720a22709a060e9e
J Robert B <k0765197@student.kirkwood.edu>
2016-11-10 14:58:41 +0000

    The 10th of November, 2016

--- VMLaHUDBiHTML/views.py
+++ VMLaHUDBiHTML/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    some_text = """
+    Hello!
+    
+    This is a formatted
+    
+        text block
+    """
+    context = {'name' : "Justin", 'tt' : some_text}
+    return render(request, 'VMLaHUDBiHTML/index.html', context) 
